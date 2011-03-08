f621549d975f0e3105d9b54b11e90d3a4b03c96e
cstricklan <contact@kraetonics.com>
2011-03-08 10:17:53 -0500

    a

--- P2/BlindedMissle.m
+++ P2/BlindedMissle.m
@@ -17,9 +17,12 @@ clear all;
 % Constants
 c0 = 299792458;
 
+%units
+nanometers = 1e-9;
+
 % Frequency we want to transmit
-lambda_0 = 980e-9 % nm;
-f0 = c0/lamda_0
+lambda_0 = 980 * nanometers; % nm;
+f0 = c0/lambda_0;
 
 % Bragg Grating Materials
 nSiN = 2.0;
@@ -28,50 +31,49 @@ erSiN = nSiN^2;
 nSiO2 = 1.5;
 erSiO2 = nSiO2^2;
 
-
+Periods = 10;
 
 %Calculate the Length of our layers.
-LSiN = lambda_0/(4*nSiN);
-LSiO2 = lambda_0/(4*nSiO2);
+LSiN = lambda_0/(4*nSiN) / nanometers;
+LSiO2 = lambda_0/(4*nSiO2) / nanometers;
 
-% We need to place a Anti-Reflective Layer on each side of the Radome
-e_radome = 12;
-e_air = 1;
-e_nonreflective = sqrt(e_radome*e_air);
 
-n_nonreflective = sqrt(e_nonreflective);
-d_nonreflective = lambda_trans/(4*n_nonreflective);
+dc = LSiN+LSiO2*nanometers; %nmeters Our critical dimension in this case is the width of one period.
+Length = ((LSiN+LSiO2)) * Periods;
 
-dc = LSiN+LSiO2; %meters Our critical dimension in this case the anti-reflectivelayer
-rNz = ceil((round(d_radome*100) + 2*round(d_nonreflective*100)))+2; %This Nz represents real world size
+rNz = ceil(Length); %This Nz represents real world size
 
 % Material Vectors Initialized at Air
 rER = ones([1 rNz]);
 rUR = ones([1 rNz]); 
 
 % Add our Materials to the model
-zstart = 1;
-zend = ceil(d_nonreflective*100);
-rER(zstart: zend) = e_nonreflective;
+nstart = 1;
+nend = nstart;
 
-zstart = zend + 1;
-zend = zstart + floor(d_radome*100);
-rER(zstart:zend) = e_radome;
-
-zstart=zend+1;
-zend = zstart + floor(d_nonreflective*100);
-rER(zstart:zend) = e_nonreflective;
-
+for n = 1: Periods
+  nend = nstart + round(LSiN)-1;
+  rER(nstart:nend) = erSiN;
+  
+  nstart = nend+1;
+  nend = round(LSiO2) -1;
+  rER(nstart:nend) = erSiO2;
+  
+  nstart = nend+1;
+end
 
 % Frequency
 
-freq_start = 0; %DC
-freq_end = 5e9;%f_trans*2; %1Ghz
+freq_start = 900*nanometers/c0; %DC
+freq_end = 1100*nanometers/c0;%f_trans*2; %1Ghz
 
-NFREQ = freq_end / 10e6; %Frequencies every 100Mhz upto 5Gz
-FREQ = linspace(900, 1100, 100) * 1e-9; %FREQ List
+NFREQ = freq_end / 100/c0; %Frequencies every 100nm
+FREQ = linspace(freq_start, freq_end, NFREQ); %FREQ List
 
-FDTD1D( dc, (d_radome+2*d_nonreflective), rER, rUR, 35000, 100, FREQ, NFREQ, 2000);
+disp(rER);
+return;
+
+FDTD1D( dc, Length, rER, rUR, -1, -1, FREQ, NFREQ, 2000);
 
 
 
